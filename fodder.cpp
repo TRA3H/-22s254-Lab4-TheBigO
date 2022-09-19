de07cc8135b17be01fda93c746f4850628826fc5
Cyrus <cyrusb10@gmail.com>
2022-09-19 02:18:06 -0700

    stupid

--- fodder.cpp
+++ fodder.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <algorithm>
 using namespace std;
 int main()
 {
@@ -9,10 +10,21 @@ int main()
     if (myFile.is_open())
     {
         string tp;
+        string chars = "*";
+
         while (getline(myFile, tp))
         {
-            tp.erase(tp.begin() + 0, tp.end() - 6);
-            cout << tp << "\n";
+            for(char c: chars){
+                tp.erase(std::remove(tp.begin(), tp.end(), c), tp.end());
+            }
+            tp.erase(0,1);
+            
+            if(tp == "Sean Hatfield\r" || tp == "Cyrus Baybay\r" || tp == "Rina Watanabe\r"){
+                cout<< tp << "\n";
+            }
+            //i actually hate my life the stupid \r was in the way of getting to what i wanted - cyrus baybay
+            
+
         }
         myFile.close();
     }
